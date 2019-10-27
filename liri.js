c36b3e956821398a87760b39d504cc2dce4d7010
Melissa Chiang <mchiang62@gmail.com>
2019-10-27 17:58:58 -0400

    switch case added

--- liri.js
+++ liri.js
@@ -5,11 +5,51 @@ var keys = require("./keys.js")
 var Spotify = require('node-spotify-api');
 var spotify = new Spotify(keys.spotify);
 
-spotify
-  .search({ type: 'track', query: 'Man in the Mirror' })
-  .then(function(response) {
-    console.log(response);
-  })
-  .catch(function(err) {
-    console.log(err);
-  });
+var axios = require('axios');
+
+var fs = require("fs");
+
+var input = process.argv;
+
+var command = input[2];
+var artist = input[3];
+
+// concert-this
+// spotify-this-song
+// movie-this
+// do-what-it-says
+
+switch (command) {
+    case "concert-this":
+        // concert function
+        break;
+
+    case "spotify-this-song":
+        // spotifiy function
+        break;
+
+    case "movie-this":
+        // movie function
+        break;
+
+    case "do-what-it-says":
+        // do function
+        break;
+
+};
+
+
+
+
+
+
+
+
+// spotify
+//   .search({ type: 'track', query: 'Man in the Mirror' })
+//   .then(function(response) {
+//     console.log(response);
+//   })
+//   .catch(function(err) {
+//     console.log(err);
+//   });
